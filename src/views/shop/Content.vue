dd6034e62b36eecb8ca744769fc7fd1bd1544d66
xiatec <1870812219@qq.com>
2022-02-15 22:02:00 +0800

    tab栏切换

--- src/views/shop/Content.vue
+++ src/views/shop/Content.vue
@@ -1,22 +1,27 @@
 <template>
   <div class="content">
     <div class="category">
-      <div class="category__item category__item--active">全部商品</div>
-      <div class="category__item">秒杀</div>
-      <div class="category__item">新鲜水果</div>
-      <div class="category__item">休闲食品</div>
-      <div class="category__item">时令蔬菜</div>
-      <div class="category__item">肉蛋家禽</div>
+      <div
+        :class="{
+          category__item: true,
+          'category__item--active': currentTab === item.tab,
+        }"
+        v-for="item in categories"
+        :key="item.name"
+        @click="() => handleCategoryClick(item.tab)"
+      >
+        {{ item.name }}
+      </div>
     </div>
     <div class="product">
-      <div class="product__item">
-        <img class="product__item__img" src="http://www.dell-lee.com/imgs/vue3/near.png" />
+      <div class="product__item" v-for="item in list" :key="item._id">
+        <img class="product__item__img" :src="item.imgUrl" />
         <div class="product__item__detail">
-          <h4 class="product__item__title">番茄250g/份</h4>
-          <p class="product__item__sales">月售10件</p>
+          <h4 class="product__item__title">{{ item.name }}</h4>
+          <p class="product__item__sales">月售{{ item.sales }}件</p>
           <p class="product__item__price">
-            <span class="product__item__yen">&yen;</span>33.6
-            <span class="product__item__origin">&yen;66.6</span>
+            <span class="product__item__yen">&yen;</span>{{ item.price }}
+            <span class="product__item__origin">&yen;{{ item.oldprice }}</span>
           </p>
         </div>
         <div class="product__number">
@@ -30,28 +35,71 @@
 </template>
 
 <script>
-export default {
-
+import { reactive, toRefs, ref, watchEffect } from "vue";
+import { get } from "../../utils/request";
+import { useRoute } from "vue-router";
+// 代码抽离
+const categories = [
+  { name: "全部商品", tab: "all" },
+  { name: "秒杀", tab: "seckill" },
+  { name: "新鲜水果", tab: "fruit" },
+];
+//有关tab相关的逻辑
+const useTabEffect = () => {
+  const currentTab = ref(categories[0].tab);
+  const handleCategoryClick = (tab) => {
+    currentTab.value = tab;
+  }
+  return {currentTab,handleCategoryClick};
 }
+
+// 右侧栏目展示逻辑
+const useShowContentEffect = (currentTab) => {
+  const route = useRoute();
+  const shopId = route.params.id;
+  const content = reactive({list:[]});
+  const getContentList = async() => {
+    const result = await get(`/api/shop/${shopId}/products`,{tab: currentTab.value});
+    if(result?.errno === 0 && result?.data?.length) {
+      content.list = result.data;
+    }
+  }
+  watchEffect(() => {getContentList()});
+  const {list} = toRefs(content);
+  return {list}
+}
+export default {
+  name: "Content",
+  setup() {
+    const { currentTab, handleCategoryClick } = useTabEffect();
+    const { list } = useShowContentEffect(currentTab);
+    return {
+      list,
+      categories,
+      handleCategoryClick,
+      currentTab,
+    };
+  },
+};
 </script>
 
 <style lang="scss" scoped>
-@import '../../style/mixins.scss';
+@import "../../style/mixins.scss";
 .content {
   display: flex;
   position: absolute;
   left: 0;
   right: 0;
   top: 1.5rem;
-  bottom: .5rem;
+  bottom: 0.5rem;
 }
 .category {
   overflow-y: scroll;
   height: 100%;
-  width: .76rem;
+  width: 0.76rem;
   background: #f5f5f5;
   &__item {
-    line-height: .4rem;
+    line-height: 0.4rem;
     text-align: center;
     font-size: 14px;
     color: #333;
@@ -66,71 +114,70 @@ export default {
   &__item {
     position: relative;
     display: flex;
-    padding: .12rem 0;
-    margin: 0 .16rem;
-    border-bottom: .01rem solid #f1f1f1;
+    padding: 0.12rem 0;
+    margin: 0 0.16rem;
+    border-bottom: 0.01rem solid #f1f1f1;
     &__detail {
       overflow: hidden;
     }
     &__img {
-      width: .68rem;
-      height: .68rem;
-      margin-right: .16rem;
+      width: 0.68rem;
+      height: 0.68rem;
+      margin-right: 0.16rem;
     }
     &__title {
       margin: 0;
-      line-height: .2rem;
-      font-size: .14rem;
+      line-height: 0.2rem;
+      font-size: 0.14rem;
       color: #333;
       @include ellipsis;
     }
     &__sales {
-      margin: .06rem 0;
-      font-size: .12rem;
+      margin: 0.06rem 0;
+      font-size: 0.12rem;
       color: #333;
     }
     &__price {
       margin: 0;
-      line-height: .2rem;
-      font-size: .14rem;
+      line-height: 0.2rem;
+      font-size: 0.14rem;
       color: #e93b3b;
     }
     &__yen {
-      font-size: .12rem;
+      font-size: 0.12rem;
     }
     &__origin {
-      margin-left: .06rem;
-      line-height: .2rem;
-      font-size: .12rem;
+      margin-left: 0.06rem;
+      line-height: 0.2rem;
+      font-size: 0.12rem;
       color: #999;
       text-decoration: line-through;
     }
     .product__number {
       position: absolute;
       right: 0;
-      bottom: .12rem;
-      &__minus, &__plus
-       {
+      bottom: 0.12rem;
+      &__minus,
+      &__plus {
         display: inline-block;
-        width: .2rem;
-        height: .2rem;
-        line-height: .16rem;;
+        width: 0.2rem;
+        height: 0.2rem;
+        line-height: 0.16rem;
         border-radius: 50%;
-        font-size: .2rem;
+        font-size: 0.2rem;
         text-align: center;
       }
       &__minus {
-        border: .01rem solid #000;
+        border: 0.01rem solid #000;
         color: #666;
-        margin-right: .05rem;
+        margin-right: 0.05rem;
       }
       &__plus {
         background: #0091ff;
         color: #fff;
-        margin-left: .05rem;
+        margin-left: 0.05rem;
       }
     }
   }
-  
 }
 </style>
